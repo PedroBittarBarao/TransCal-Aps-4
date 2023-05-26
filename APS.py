1ce51a6a0ec629ab4cae7503f14b611ef41097c0
Pedrobittarbarao <pedrobaraocontas@gmail.com>
2023-05-26 08:51:26 -0300

    Add Jacobi

--- APS.py
+++ APS.py
@@ -19,3 +19,25 @@ def gauss_seidel_solver(max_i,tol,K,F):
         x = x_new.copy()
 
     return x_new, erro,i
+
+def jacobi_solve(max_i,tol,K,F):
+    n = len(F)
+    x = np.array(n*[0])
+    x_new = np.zeros(n)
+    for i in range(max_i):
+        for j in range(n):
+            x_new[j] = (F[j] - np.dot(K[j,:],x) + K[j,j]*x[j])/K[j,j]
+            
+        all_under = True
+        for j in range(n):
+            erro = abs((x_new[j]-x[j])/x_new[j])
+            if erro > tol:
+                all_under = False
+                break
+        if all_under:
+            break
+        x = x_new.copy()
+
+    return x_new, erro,i
+
+
